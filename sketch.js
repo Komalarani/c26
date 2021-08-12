26e0d4f2be423cce92521fb8b8bb0a5cbe9dcc27
komalarani <dellmypc2020@gmail.com>
2021-08-12 16:43:47 +0530

    log7

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log7;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -23,6 +24,7 @@ function setup(){
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
+    log7 = new Log(900,300,500,PI);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
@@ -58,6 +60,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log7.display();
 
     bird.display();
     platform.display();
